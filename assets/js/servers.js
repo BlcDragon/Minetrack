fc9f90f95689079599602f15e1e2a030298bf1cd
Blc <fenix785632@gmail.com>
2022-01-16 00:13:31 +0500

    feat: russian language

--- assets/js/servers.js
+++ assets/js/servers.js
@@ -284,9 +284,9 @@ export class ServerRegistration {
       <div class="column column-status">
         <h3 class="server-name"><span class="${this._app.favoritesManager.getIconClass(this.isFavorite)}" id="favorite-toggle_${this.serverId}"></span> ${this.data.name}</h3>
         <span class="server-error" id="error_${this.serverId}"></span>
-        <span class="server-label" id="player-count_${this.serverId}">Players: <span class="server-value" id="player-count-value_${this.serverId}"></span></span>
-        <span class="server-label" id="peak_${this.serverId}">${this._app.publicConfig.graphDurationLabel} Peak: <span class="server-value" id="peak-value_${this.serverId}">-</span></span>
-        <span class="server-label" id="record_${this.serverId}">Record: <span class="server-value" id="record-value_${this.serverId}">-</span></span>
+        <span class="server-label" id="player-count_${this.serverId}">Игроков: <span class="server-value" id="player-count-value_${this.serverId}"></span></span>
+        <span class="server-label" id="peak_${this.serverId}">Суточный максимум: <span class="server-value" id="peak-value_${this.serverId}">-</span></span>
+        <span class="server-label" id="record_${this.serverId}">Рекорд: <span class="server-value" id="record-value_${this.serverId}">-</span></span>
         <span class="server-label" id="version_${this.serverId}"></span>
       </div>
       <div class="column column-graph" id="chart_${this.serverId}"></div>`
